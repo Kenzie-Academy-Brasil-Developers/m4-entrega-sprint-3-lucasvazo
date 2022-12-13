bfc376c755e7f78299a8896a54e20fbb9bd8ad8d
Lucas Vaz <lucasvazo@hotmail.com>
2022-12-12 22:27:44 -0300

    build: directories

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import app from "./app";
 import "dotenv/config"
 import { startDatabase } from "./database";
 
-export default app.listen(3000, () => {
-    startDatabase();
+app.listen(3000, () => {
     console.log("Server running");
+    startDatabase();
 });
